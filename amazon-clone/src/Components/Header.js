b59850fb95d413f2c574a0259dfda2543b872d63
jenobpj <jenobgit@gmail.com>
2021-12-31 16:21:19 -0500

    import shopping cart icon and apply styles

--- amazon-clone/src/Components/Header.js
+++ amazon-clone/src/Components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Header.css";
 import SerachIcon from "@material-ui/icons/Search";
-
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 function Header() {
   return (
     <div className="header">
@@ -26,6 +26,10 @@ function Header() {
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
