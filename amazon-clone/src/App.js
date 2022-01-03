c783066a66cab0eb65f2c3100503b63b253ca601
jenobpj <jenobgit@gmail.com>
2022-01-02 21:05:10 -0500

    implement the routes

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Home />
+            </>
+          }
+        />
+      </Routes>
     </div>
   );
 }
