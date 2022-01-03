ad8223ff8f54a8029b13c318794e4fb35885863c
jenobpj <jenobgit@gmail.com>
2022-01-03 18:27:40 -0500

    add checkout components and link to main page

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,29 +2,15 @@ import "./App.css";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import { Routes, Route } from "react-router-dom";
+import Checkout from "./Components/Checkout";
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Header />
-              <Home />
-            </>
-          }
-        />
-        <Route
-          path="/checkout"
-          element={
-            <>
-              <Header />
-              <p>Iam checkout</p>
-            </>
-          }
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </div>
   );
--- amazon-clone/src/Components/Checkout.js
+++ amazon-clone/src/Components/Checkout.js
@@ -1,6 +1,21 @@
 import React from "react";
 import "./Checkout.css";
 function Checkout() {
-  return <div></div>;
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://mk0adespressoj4m2p68.kinstacdn.com/wp-content/uploads/2020/05/image8-3.png"
+        />
+        <div>
+          <h2 className="checkout__title">Your shopping Basket</h2>
+        </div>
+      </div>
+      <div className="checkout__right">
+        <h2>Subtotal</h2>
+      </div>
+    </div>
+  );
 }
 export default Checkout;
