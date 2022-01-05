cb764a8479c889f52ed4727cd9397484dec50614
jenobpj <jenobgit@gmail.com>
2022-01-04 22:58:18 -0500

    add fake data into checkout

--- amazon-clone/src/Components/Checkout.js
+++ amazon-clone/src/Components/Checkout.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./Checkout.css";
 import Subtotal from "./Subtotal";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -12,6 +15,29 @@ function Checkout() {
         />
         <div>
           <h2 className="checkout__title">Your shopping Basket</h2>
+          <CheckoutProduct
+            id="12123"
+            title="just for test"
+            image="https://m.media-amazon.com/images/I/61pMwykTQYL._SX3000_.jpg"
+            price="23.45"
+            rating={5}
+          />
+          <CheckoutProduct
+            id="12123"
+            title="just for test"
+            image="https://m.media-amazon.com/images/I/61pMwykTQYL._SX3000_.jpg"
+            price="23.45"
+            rating={5}
+          />
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
       <div className="checkout__right">
