584c5f7de2f9363bdf97504d751c48eae853a328
jenobpj <jenobgit@gmail.com>
2022-01-05 17:52:07 -0500

    fix the firebase bug

--- amazon-clone/src/Components/firebase.js
+++ amazon-clone/src/Components/firebase.js
@@ -1,5 +1,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-import firebase from "firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyCP57jz8b3KXuHCqBBjCmtNuDftjQepb0M",
   authDomain: "clone-5db27.firebaseapp.com",
@@ -10,9 +12,10 @@ const firebaseConfig = {
   measurementId: "G-PRXKLL49LB",
 };
 
-const firebaseApp = firebase.initalizeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
+// Use these for db & auth
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 
-export { db, auth };
+export { auth, db };
