2d1c9a5ecdd9864569c00f95b977f73f10f39ee9
jenobpj <jenobgit@gmail.com>
2022-01-05 15:04:31 -0500

    add login component and apply styles

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -3,14 +3,32 @@ import Header from "./Components/Header";
 import Home from "./Components/Home";
 import { Routes, Route } from "react-router-dom";
 import Checkout from "./Components/Checkout";
+import Login from "./Components/Login";
 
 function App() {
   return (
     <div className="app">
-      <Header />
+      {/* <Header /> */}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<Checkout />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Home />
+            </>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/checkout"
+          element={
+            <>
+              <Header />
+              <Checkout />
+            </>
+          }
+        />
       </Routes>
     </div>
   );
