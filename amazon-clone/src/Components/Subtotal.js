8c80bcfc7ba8f6bb02cdacdabaf8036ccf37b0f7
jenobpj <jenobgit@gmail.com>
2022-01-04 21:54:39 -0500

    add function for subtotal

--- amazon-clone/src/Components/Subtotal.js
+++ amazon-clone/src/Components/Subtotal.js
@@ -1,13 +1,15 @@
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
 function Subtotal() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="subtotal">
       <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              subtotal(0 items):<strong>0</strong>
+              subtotal({basket.length} items):<strong>0</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order a contains gift
