d66f95bf92de74e98b7815a5dc29597b7dab6900
jenobpj <jenobgit@gmail.com>
2022-01-06 20:59:41 -0500

    install axios and add payment submit function

--- amazon-clone/src/Components/Payment.js
+++ amazon-clone/src/Components/Payment.js
@@ -1,25 +1,51 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import Checkout from "./Checkout";
+import { Link, useNavigate } from "react-router-dom";
 import CheckoutProduct from "./CheckoutProduct";
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import CurrencyFormat from "react-currency-format";
 import { getBasketTotal } from "../reducer";
+import axios from "./axios";
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
-
+  const navigate = useNavigate();
   const stripe = useStripe();
   const elements = useElements();
   const [succeeded, setSucceeded] = useState(false);
   const [processing, setProcessing] = useState("");
   const [error, setError] = useState(null);
   const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState(true);
+
+  useEffect(() => {
+    //generate the special stripe which allows us to charge a customer
+    const getClientSecret = async () => {
+      const response = await axios({
+        method: "post",
+        //stripe expects the total in a currencies subunits
+        url: `/payments/create?total=${getBasketTotal(basket) * 100}`,
+      });
+      setClientSecret(response.data.clientSecret);
+    };
+    getClientSecret();
+  }, [basket]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     setProcessing(true);
+    const payload = await stripe
+      .confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      })
+      .then(({ paymentIntent }) => {
+        setSucceeded(true);
+        setError(null);
+        setProcessing(false);
+        navigate("/orders");
+      });
   };
   const handleChange = (event) => {
     //Listen for changes in the cardElement
