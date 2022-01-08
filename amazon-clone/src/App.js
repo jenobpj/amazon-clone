1794d2b51faf41292eb885ef027d0542e7ba4cfd
jenobpj <jenobgit@gmail.com>
2022-01-07 22:09:13 -0500

    delete error pages

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -5,14 +5,8 @@ import Home from "./Components/Home";
 import { Routes, Route } from "react-router-dom";
 import Checkout from "./Components/Checkout";
 import Login from "./Components/Login";
-import Payment from "./Components/Payment";
 import { auth } from "./Components/firebase";
 import { useStateValue } from "./Components/StateProvider";
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
-const promise = loadStripe(
-  "pk_test_51JuQa3F6hhx3MSQzoQhTomHo9PiuQhtIbJenvOa7TFpbWvRjJmfo73awt2CzcHIBgTnv3nZSi7ZCo83KoQKMGzJy00CA7z0o7E"
-);
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -54,17 +48,6 @@ function App() {
             <>
               <Header />
               <Checkout />
-            </>
-          }
-        />
-        <Route
-          path="/payment"
-          element={
-            <>
-              <Header />
-              <Elements stripe={promise}>
-                <Payment />
-              </Elements>
             </>
           }
         />
